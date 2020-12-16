a110f670650eaf5030ba0e1da27b7c84f007c4d9
mdasad7827 <mohaammadasad1996@gmail.com>
2020-12-16 18:59:09 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,114 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector')
+const { connection } = require("./connector");
+const covidTallyModel = connection;
 
+app.get("/totalRecovered", async (req, res) => {
+  const resDoc = await covidTallyModel.aggregate([
+    {
+      $group: {
+        _id: "total",
+        recovered: { $sum: "$recovered" },
+      },
+    },
+  ]);
+  const finalResult = resDoc[0];
+  res.send({ data: finalResult });
+});
 
+app.get("/totalDeath", async (req, res) => {
+  const resDoc = await covidTallyModel.aggregate([
+    {
+      $group: {
+        _id: "total",
+        death: { $sum: "$death" },
+      },
+    },
+  ]);
+  const finalResult = resDoc[0];
+  res.send({ data: finalResult });
+});
 
+app.get("/totalActive", async (req, res) => {
+  const resDoc = await covidTallyModel.aggregate([
+    {
+      $group: {
+        _id: "total",
+        recovered: { $sum: "$recovered" },
+        infected: { $sum: "$infected" },
+      },
+    },
+  ]);
 
+  const finalResult = resDoc[0];
+  res.send({
+    data: {
+      _id: "total",
+      active: finalResult.infected - finalResult.recovered,
+    },
+  });
+});
 
+app.get("/hotspotStates", async (req, res) => {
+  const resDoc = await covidTallyModel.aggregate([
+    {
+      $project: {
+        state: "$state",
+        rate: {
+          $round: [
+            {
+              $divide: [
+                {
+                  $subtract: ["$infected", "recovered"],
+                },
+                "infected",
+              ],
+            },
+            5,
+          ],
+        },
+      },
+    },
+    {
+      $match: {
+        rate: { $gt: 0.1 },
+      },
+    },
+  ]);
+  res.send({
+    data: resDoc,
+  });
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.get("/healthyStates", async (req, res) => {
+  const resDoc = await covidTallyModel.aggregate([
+    {
+      $project: {
+        state: "$state",
+        mortality: {
+          $round: [
+            {
+              $divide: ["$death", "$infected"],
+            },
+            5,
+          ],
+        },
+      },
+    },
+    {
+      $match: {
+        mortality: { $lt: 0.005 },
+      },
+    },
+  ]);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
